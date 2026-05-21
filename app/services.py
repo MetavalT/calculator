5623df8d2fa63885db777e019bb38119a9e0b0bc
MetavalIT <mohit.sharma@metaval.com>
2026-05-21 16:36:20 +0530

    commit pta nhi chal rha h kya likhu to date hi thik h: 21st May 2026.

--- app/services.py
+++ app/services.py
@@ -18,10 +18,7 @@ def evaluate_formula(
 
         value = value_data['value']
 
-        selected_unit = value_data.get(
-            'unit',
-            expected_unit
-        )
+        selected_unit = value_data['unit']
 
         config = variable_config[variable_name]
 
@@ -43,12 +40,16 @@ def evaluate_formula(
 
         processed_values[variable_name] = converted_value
 
-    final_expression = expression.replace("^", "**")
+    final_expression = expression
 
+    # ^ ko python power me convert karo
+    final_expression = final_expression.replace("^", "**")
+
+    # sqrt support
     final_expression = final_expression.replace(
-    "sqrt",
-    "math.sqrt"
-)
+        "sqrt",
+        "math.sqrt"
+    )
 
     for variable, value in processed_values.items():
 
