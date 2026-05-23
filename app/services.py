b8e5c78e7030dc5bc91079de6715c3b8aefa5007
MetavalIT <mohit.sharma@metaval.com>
2026-05-23 17:10:18 +0530

    Date: 23rd May 2026

--- app/services.py
+++ app/services.py
@@ -1,5 +1,69 @@
+# from app.utils import convert_to_expected
+# import math
+
+
+# def evaluate_formula(
+#     expression,
+#     variables,
+#     variable_config
+# ):
+
+#     processed_values = {}
+
+#     for variable_name, value_data in variables.items():
+
+#         value = value_data['value']
+
+#         config = variable_config[variable_name]
+
+#         expected_unit = config['expected_unit']
+
+#         variable_type = config['variable_type']
+
+#         # agar unit nahi aayi
+#         selected_unit = value_data.get(
+#             'unit',
+#             expected_unit
+#         )
+
+#         converted_value = convert_to_expected(
+
+#             value,
+#             variable_type,
+#             selected_unit,
+#             expected_unit
+#         )
+
+#         processed_values[variable_name] = converted_value
+
+#     final_expression = expression
+
+#     for variable, value in processed_values.items():
+
+#         final_expression = final_expression.replace(
+#             variable,
+#             str(value)
+#         )
+
+#     result = eval(
+#         final_expression,
+#         {"sqrt": math.sqrt}
+#     )
+
+#     if result == int(result):
+#         result = int(result)
+#     else:
+#         result = round(result, 4)
+
+#     return {
+#         "value": result,
+#         "unit": ""
+#     }
+
+
+
 from app.utils import convert_to_expected
-import math
+from asteval import Interpreter
 
 
 def evaluate_formula(
@@ -45,10 +109,10 @@ def evaluate_formula(
             str(value)
         )
 
-    result = eval(
-        final_expression,
-        {"sqrt": math.sqrt}
-    )
+    # SAFE EVALUATION
+    aeval = Interpreter()
+
+    result = aeval(final_expression)
 
     if result == int(result):
         result = int(result)
